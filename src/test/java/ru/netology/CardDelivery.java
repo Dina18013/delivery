e2849887642686d987372f6b7e6f184b2493bff8
Надежда Волынец <vnv18013@gmail.com>
2022-06-29 22:13:02 +0300

    test changed

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -1,13 +1,17 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
+import static java.time.Duration.ofSeconds;
 
 public class CardDelivery {
+
+    SelenideElement notification = $x("//div[@data-test-id='notification']");
 
     @BeforeEach
     public  void setUp() {
@@ -16,8 +20,18 @@ public class CardDelivery {
 
     @Test
     public void ShouldCardDelivery () {
+        $x(".//span[@data-test-id='city']//input").setValue("Москва");
+       // $x(".//span[@data-test-id=date]//input").setValue("02.07.2022");
+        $x(".//span[@data-test-id='name']//input").setValue("Иванов Виктор");
+        $x(".//span[@data-test-id='phone']//input").setValue("+79101234567");
+        $x(".//label[@data-test-id='agreement']").click();
+        $x(".//span[contains(text(),'Забронировать')]").click();
+        notification.should(visible, ofSeconds(15));
+        notification.$x(".//div[@class='notification__title']").should(text("Успешно!"));
+        notification.$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на "
+                + $x(".//span[@data-test-id='date']//input").getValue()));
+        notification.$x(".//button").click();
+        notification.should(hidden);
 
     }
-
-
 }
